b5e58a10ec9547f6a99729f0f515ce3393a201f5
Matthew Burke <matthelb@usc.edu>
2014-03-08 02:59:29 -0800

    Use tweet generator to generate tweets based on recent tweets

---
+++ config.php.ex
@@ -0,0 +1,7 @@
+<?php
+define('CONSUMER_KEY', '');
+define('CONSUMER_SECRET', '');
+define('OAUTH_TOKEN', '');
+define('OAUTH_TOKEN_SECRET', '');
+define('DATABASE_NAME', __DIR__ . '/db/Universities.sqlite');
+?>
