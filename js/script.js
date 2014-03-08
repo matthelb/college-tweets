aef9d70a0abad94062c05dc7115be9dca8b01185
Ananth Mohan <ananthmo@usc.edu>
2014-03-08 15:08:55 -0800

    Added lower case checking, hashtag linking

--- js/script.js
+++ js/script.js
@@ -12,6 +12,11 @@ $(document).ready(function(){
 		$('#college').text($('#selectcollege').find(':selected').text());
 		$.get('./ajax/tweet.php', {'college' : $('#selectcollege').find(':selected').text()}, function(data) {
 			if (data.success) {
+				var tweet = $("<div>");
+				var tweetStr = data.tweet;
+				var i = 0;
+				while(i < tweetStr.length)
+				tweet.append($("<span>")
 				$('#college-tweet').text(data.tweet);
 			}
 		});
