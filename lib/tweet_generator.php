ef9c0ff9472302d289494121ec2edfa2c8ba7dbf
Ananth Mohan <ananthmo@usc.edu>
2014-03-08 03:56:37 -0800

    merging

--- lib/tweet_generator.php
+++ lib/tweet_generator.php
@@ -1,8 +1,8 @@
 <?php
 	class Tweet_Generator {
 		public $map = array();
-		public $separators = array('#',',','.','!','?','@','$','%','^','&','*','(',')','-','+','=','"',':');
-		public $spacers = array('.', ',',';',':','?','!');
+		public $separators = array(',','.','!','?','$','%','^','&','*','(',')','-','+','=','"',':',';');
+		public $spacers = array('.', ',',';','?','!');
 
 		public function add_tweet($t){
 			$m = $this->map;
@@ -40,15 +40,16 @@
 					}
 					else break;
 				}
-				$size += sizeof($word);
+				$size += sizeof($word)+1;
 				array_push($words, $word);
 				$seed = $word;
 			}
+			if(in_array($words[0], $this->separators)) array_splice($words, 0, 1);
 			for($i = 0;$i<sizeof($words);$i++) {
 				if($i > 0){
-					if($words[$i-1] == '.' || $words[$i-1] == ','){
-						$result .= " ".$words[$i];
-					}
+
+				} else {
+					$result .= $words[$i];
 				}
 			}
 			return $result;
@@ -56,7 +57,7 @@
 
 		private function get_words_from_string($t){
 			foreach($this->separators as $delimiter){
-				$t = str_replace($delimiter, " ".$delimiter." ", $t);
+				$t = str_replace($delimiter, " " . $delimiter . " ", $t);
 			}
 			$words = explode(' ', $t);
 			for($i = 0;$i<sizeof($words);$i++) {
