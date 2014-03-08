cdaa234be28b180b4b0dc113209cdc994b70da4e
Matthew Burke <matthelb@usc.edu>
2014-03-08 14:27:45 -0800

    Delete config.php
    Remove OAuth secret from repo

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('CONSUMER_KEY', 'KmN6DQ3NuO9JW4JvqPDxw');
-define('CONSUMER_SECRET', 'BWXWapJOo4zvQ5h1h4ZbktgGelSbmB1jhnFUxtojk');
-define('OAUTH_TOKEN', '328791434-03C3bhTw4vhQ5BtfvzOFuJzaBdcR2xqTUQF9NAqg');
-define('OAUTH_TOKEN_SECRET', 'QTsIi0Bi3zqIHomJnUvKxxIs3U46acXCBd6TqKryUEs3o');
-define('DATABASE_NAME', __DIR__ . '/db/Universities.sqlite');
-?>
