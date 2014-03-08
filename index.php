10a93d675afadd0360616b6da6fde7b0a4d9f3c1
Matthew Burke <matthelb@usc.edu>
2014-03-08 01:18:40 -0800

    Add tweet retrieving function

---
+++ index.php
@@ -0,0 +1,4 @@
+<?php
+require_once(__DIR__ . '/lib/tweet_generator.php');
+echo generateTweet('University of Southern California');
+?>
---
+++ lib/tweet_generator.php
@@ -0,0 +1,56 @@
+<?php
+require_once(__DIR__ . '/TwitterOAuth/TwitterOAuth.php');
+require_once(__DIR__ . '/TwitterOAuth/Exception/TwitterException.php');
+require_once(__DIR__ . '/../config.php');
+
+use TwitterOAuth\TwitterOAuth;
+
+$db = new SQLite3(DATABASE_NAME);
+
+$config = array(
+    'consumer_key' => CONSUMER_KEY,
+    'consumer_secret' => CONSUMER_SECRET,
+    'oauth_token' => OAUTH_TOKEN,
+    'oauth_token_secret' => OAUTH_TOKEN_SECRET,
+    'output_format' => 'object'
+);
+
+$t = new TwitterOAuth($config);
+
+function getCollege($id) {
+	global $db;
+	$result;
+	$statement = $db->prepare('SELECT * FROM colleges WHERE ' .
+		((is_int($id)) ? 'rowid' : 'name') . ' = :id');
+	$statement->bindValue(':id', $id, (is_int($id)) ? SQLITE3_INTEGER : SQLITE3_TEXT);
+	$result = $statement->execute()->fetchArray();
+	return $result;
+}
+
+function getTweetStrings($collegeId, $maxId=-1, $count=100, $type='recent') {
+	global $t;
+	$college = getCollege($collegeId);
+	$latitude = $college['latitude'];
+	$longitude = $college['longitude'];
+	$radius = $college['radius'];
+	$params = array(
+		'geocode' => "$latitude,$longitude,$radius" . 'mi',
+		'count' => $count,
+		'result_type' => $type
+	);
+	if ($maxId > -1) {
+		$params['max_id'] = $maxId;
+	}
+	$response = $t->get('search/tweets', $params);
+	$tweetStrings = array();
+	$tweets = $response->statuses;
+	foreach ($tweets as $tweet) {
+		array_push($tweetStrings, $tweet->text);
+	}
+	return array('strings' => $tweetStrings, 'lastId' => $tweets[sizeof($tweets)-1]->id_str);
+}
+
+function generateTweet($collegeId) {
+	return getTweetStrings($collegeId)['strings'][0];
+}
+?>
