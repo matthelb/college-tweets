89a907092f1d29150d38e0f2748f02df337c69a9
Ananth Mohan <ananthmo@usc.edu>
2014-03-08 02:39:42 -0800

    Merging

--- index.php
+++ index.php
@@ -4,14 +4,13 @@
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 <head>
-	<script src="tweet_generator.js"></script>
-	<script src="test.js"></script>
 </head>
-<body onload="test()">
+<body>
 	<?php
 		require_once('tweet_generator.php');
 		$generator = new Tweet_Generator();
-		$generator->add_tweet('Hello. My name is Ananth. Or it might be?? You\'ll be scared');
+		$generator->add_tweet('Hello. Ananth here. Hopefully, this works! But, I don\'t know if it will!');
+		$generator->add_tweet('Please work!! Hello please work... I really hope so, otherwise it\'s going to be annoying.');
 		$generator->generate_tweet();
 	?>
 </body>
