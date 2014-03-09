87af06dff59d03a270300cb7df7ce87c44e5dd26
Matthew Burke <matthelb@usc.edu>
2014-03-09 09:39:04 -0700

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('CONSUMER_KEY', '31MUzaoeRDeRHyd9NSbKfQ');
-define('CONSUMER_SECRET', 'sUwl9Mwa9dlmBFPoWffHRoWsHFyYiOgwbUtMcbwx18');
-define('OAUTH_TOKEN', '32164906-5Cnz3RvVVLmKszWA4QVgZI1IgUR6YpmmpeJ48kkU5');
-define('OAUTH_TOKEN_SECRET', 'cqyNU5s5LtT1ftgIIPbxwNZkqM5EuLB2dGCBkaXmZ94Wx');
-define('DATABASE_NAME', __DIR__ . '/db/Universities.sqlite');
-?>
