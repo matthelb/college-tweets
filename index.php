d51b9a289a33b61fb77ac1fe8b4313d81cfe3756
Rochelle <rwillard@usc.edu>
2014-03-08 16:15:04 -0800

    layout

--- index.php
+++ index.php
@@ -12,37 +12,53 @@
         <link rel="stylesheet" href="css/bootstrap.min.css">
         <style>
             body {
-                padding-top: 50px;
-                padding-bottom: 20px;
+                padding-top: 0px;
+                padding-bottom: 20px;   
+                background-image: url("img/bkg.png");
+                background-repeat:repeat-x;
             }
         </style>
         <link rel="stylesheet" href="css/bootstrap-theme.min.css">
         <link rel="stylesheet" href="css/main.css">
+        <link rel="stylesheet" href="css/logo.css">
 
         <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
     </head>
     <body>
         <div id="main">
-          <h1>#college</h1>
-            <select id="selectcollege">
-              <option value="">Select a college</option> 
-            </select>
-            <div id="submitbutton" class="btn btn-md btn-default">Submit</div>
-          <h4>Here's a typical tweet from a student at <span id="college">...</span></h4>
-          <p id="college-tweet"></p>
+          <p id="title_bar"><img src="img/title.png" /></p>
+            <div id="columns">
+            <div id="left_c">
+                <img src="img/twitter_left_bar.png" />
+                <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            </div>
+            <div id="right_c">          
+                <select id="selectcollege">
+                  <option value="">Select a college</option> 
+                </select>
+                <div id="submitbutton" class="btn btn-md btn-default">Submit</div>
+              <h4>Here's a typical tweet from a student at <span id="college">...</span></h4>
+              <p id="college-tweet"></p>
+            </div>
+            </div>
+
+        	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
+            <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
+
+            <script src="js/vendor/bootstrap.min.js"></script>
+            <script src="js/main.js"></script>
+
+            <script>
+                var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+                (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+                g.src='//www.google-analytics.com/ga.js';
+                s.parentNode.insertBefore(g,s)}(document,'script'));
+            </script>
         </div>
-
-    	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
-        <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
-
-        <script src="js/vendor/bootstrap.min.js"></script>
-        <script src="js/main.js"></script>
-
-        <script>
-            var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
-            (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
-            g.src='//www.google-analytics.com/ga.js';
-            s.parentNode.insertBefore(g,s)}(document,'script'));
-        </script>
     </body>
 </html>
