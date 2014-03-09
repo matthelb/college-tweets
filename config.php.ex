e3c5fe54ba69eb6f6b828b883dfc22d4d962ef9c
Joyce Yan <joyceyan@usc.edu>
2014-03-08 21:54:08 -0800

    modified some things

--- config.php.ex
+++
@@ -1,7 +0,0 @@
-<?php
-define('CONSUMER_KEY', '');
-define('CONSUMER_SECRET', '');
-define('OAUTH_TOKEN', '');
-define('OAUTH_TOKEN_SECRET', '');
-define('DATABASE_NAME', __DIR__ . '/db/Universities.sqlite');
-?>
