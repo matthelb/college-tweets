95cf1028fc3033b39ce82b6c9303ad2bca7cf655
Joyce Yan <joyceyan@usc.edu>
2014-03-08 21:57:08 -0800

    minor change

--- index.php
+++ index.php
@@ -59,7 +59,6 @@
 
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.1/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.10.1.min.js"><\/script>')</script>
-
     <script src="js/vendor/bootstrap.min.js"></script>
     <script src="js/twitter-text.js"></script>
     <script src="js/main.js"></script>
@@ -68,7 +67,6 @@
     <script>
         $(document).ready(function() { $("#selectcollege").select2(); });
     </script>
-
     <script>
         var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
         (function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
